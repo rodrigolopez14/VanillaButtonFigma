5bc43e94b6eadd30e406285c6450c5ad001d31ef
rodrigolopez14 <rodrigo.lopez@14agency.com>
2022-02-28 16:28:38 +0100

    Removed file to avoid slots not defined, already in index file of library

--- public/Accordion/avoidSlots.js
+++
@@ -1,4 +0,0 @@
-import { CUSTOM_ELEMENT_NAME } from "../Accordion/constants.js"
-const styleSheet = document.createElement("style")
-styleSheet.innerText = `${CUSTOM_ELEMENT_NAME}:not(:defined) { display:none }`
-document.head.appendChild(styleSheet)
